deb0d0761594793fd9fc5081b9a996cf8ddc3dff
axelcorreae <axellink4@gmail.com>
2024-03-03 16:51:19 -0600

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import imgAxelLogo from './imagenes/axel-logo.jpg'
 
 function App() {
   return (
     <div className='App'>
-      <div className=''>
+      <div className='contenedor-logo-axel'>
+        <img 
+        src={imgAxelLogo}
+        className='logo-imagen'
+        alt='Logo de Axel' />
+      </div>
+      <div className='contenedor-calculadora'>
         
       </div>
     </div>
